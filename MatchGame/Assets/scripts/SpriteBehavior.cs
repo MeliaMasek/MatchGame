348f4d3a369dd3a6f7e80de1b507cf95e0d2921d
Melia Masek <Masekmelia@gmail.com>
2022-09-14 09:54:22 -0600

    removed matchgame assets

--- MatchGame/Assets/scripts/SpriteBehavior.cs
+++ MatchGame/Assets/scripts/SpriteBehavior.cs
@@ -1,13 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 [RequireComponent(typeof(SpriteRenderer))]
 public class SpriteBehavior : MonoBehaviour
 
 {
-    private SpriteRenderer rendererObj;
-
-    private void Start()
+    private SpriteRenderer rendererObj; 
+    private void Awake()
     {
         rendererObj = GetComponent<SpriteRenderer>();
     }
